fd575d1f59ca50cf64698e44419ae2ec154e7908
Scott Grogan <sgrogan@navigatingcancer.com>
2016-09-24 13:26:26 -0700

    Adding the Mexican sprite

--- src/scripts/sprites/index.js
+++ src/scripts/sprites/index.js
@@ -1,10 +1,12 @@
 import trumpSprite from './trumpSprite';
 import BrickSprite from './BrickSprite';
+import MexicanSprite from './MexicanSprite';
 
 module.exports = {
   loadResources: function loadResources (loader) {
     trumpSprite.loadResource(loader);
     BrickSprite.loadResource(loader);
+    MexicanSprite.loadResource(loader);
   },
 
   trump: function playertrump (game, x, y) {
@@ -13,5 +15,9 @@ module.exports = {
 
   brick: function brickSprite (game, x, y) {
     return new BrickSprite(game, x, y);
+  },
+
+  mexican: function mexicanSprite (game, x, y) {
+    return new MexicanSprite(game, x, y);
   }
 };

c43c20fc1ff0d64adb8ded46b84c11a072262f55
Ben McEldowney <bmceldowney@weareratio.com>
2016-09-25 11:33:51 -0700

    calculating remaining space as score

--- src/scripts/states/GameOver.js
+++ src/scripts/states/GameOver.js
@@ -2,9 +2,14 @@ import _State from './_State';
 import Fonts from '../fonts';
 
 export default class GameOver extends _State {
+  init (params) {
+    this.score = params.score;
+  }
+
   create () {
     this.titleText = this.createTitleText(this.world.centerX, 40);
-    this.actionText = this.createActionText(this.world.centerX, 80);
+    this.scoreText = this.createScoreText(this.world.centerX, 80);
+    this.actionText = this.createActionText(this.world.centerX, 160);
     this.time.events.loop(400, () => {
       this.actionText.visible = Boolean(!this.actionText.visible);
     });
@@ -12,6 +17,10 @@ export default class GameOver extends _State {
 
   createTitleText (x, y) {
     return Fonts.display(this.game, x, y, 'Yer done!', 12, 'center', this.world);
+  }
+
+  createScoreText (x, y) {
+    return Fonts.display(this.game, x, y, 'You scored ' + this.score, 12, 'center', this.world);
   }
 
   createActionText (x, y) {

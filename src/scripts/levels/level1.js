c61a6f40e19ab98b7f7bc8cb7bab207606a0ce7f
Gabriel Edwards <gabrieledwards@ymail.com>
2016-09-25 17:19:17 -0700

    begin level design for 1 and 2

--- src/scripts/levels/level1.js
+++ src/scripts/levels/level1.js
@@ -1,11 +1,16 @@
 module.exports = {
   humans: [
-    { x: 80,  y: 64  },
+    //{ x: 80,  y: 64  },
     { x: 160, y: 64  },
     { x: 80,  y: 128 }
   ],
   enemies: [
-    { x: 240, y: 192 }
+    { x: 240, y: 192 },
+    { x: 80, y: 64}
   ],
-  player: { x: 160, y: 128 }
+  player: { x: 160, y: 128 },
+  brickPositions: [
+    { x: 96,  y: 96}
+
+  ]
 };
